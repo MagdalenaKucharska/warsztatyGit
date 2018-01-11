d713869266cc0a6f0bb04640c728b006d34d0fe2
Maciej Koczur <maciek.koczur@op.pl>
2018-01-11 18:42:17 +0100

    commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+	string line;
+	signed long long suma = 0;
+	ifstream myfile ("in1.txt");
+	
+	if (myfile.is_open()){
+		
+		while (getline (myfile, line)){
+			
+			if (line[0] = '+'){
+				
+			}
+			else if (line[1] ==
+		}
+	}
+}
